2de0979ea53477009fd89ad991a554c422d06957
AlonzoRon <ron.alonzo@eee.upd.edu.ph>
2019-01-23 01:34:07 +0800

    updated character object program. HP system seems to work fine.

--- Game_code/Character.py
+++ Game_code/Character.py
@@ -6,7 +6,7 @@ class Character:
         self.hp = hp
         self.max_hp = hp
         self.name = name
-        print("You have have created a character with", self.max_hp, hp)
+        print("You have have created a character with", self.max_hp, "hp")
         print("his/her name is", self.name)
 
     def hp_status(self):
@@ -14,12 +14,24 @@ class Character:
 
     def damage(self, damage_val):
         self.hp -= damage_val
+        print("You have taken {} damage".format(damage_val))
+
         if self.hp < 0:
+            self.hp = 0
             print("You have died.")
+            print("Game over.")
+            exit()
             # possibly insert some death consequence here
+            # for the meantime, game will end
+
+        self.hp_status()
 
     def heal(self, heal_val):
+        print("You have been healed {} hp".format(heal_val))
+
         if self.hp + heal_val > self.max_hp:
             self.hp = self.max_hp
         else:
             self.hp += heal_val
+
+        self.hp_status()
