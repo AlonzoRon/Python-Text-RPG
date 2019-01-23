506e8f83ba123a05ce297a76c21b8bf556fc3c08
AlonzoRon <ron.alonzo@eee.upd.edu.ph>
2019-01-24 00:11:54 +0800

    created print_out function for scroll effect ease of use

--- Game_code/Main.py
+++ Game_code/Main.py
@@ -3,19 +3,20 @@ from Character import Character
 from Slow_text import Typewriter
 
 
+def print_out(string, speed=0.05):
+    message_object = Typewriter(string, speed)
+    message_object.text_scroll()
+
+
 def main():
 
     # the way I'm creating messages here is still inefficient
     # just for testing purposes
 
-    intro_message = "Hello Adventurer! Your Journey starts here\n"
-    intro_message = Typewriter(intro_message)
-    intro_message.text_scroll()
+    # update: created a function print_out for text printing
 
-    second_message = "What would your name be?\n"
-    second_message = Typewriter(second_message, typing_speed=0.20)
-    second_message.text_scroll()
-
+    print_out("Hello Adventurer! Your Journey starts here\n")
+    print_out("What would your name be?\n", 0.2)
     name = str(input())
 
     character_1 = Character(100, name)
